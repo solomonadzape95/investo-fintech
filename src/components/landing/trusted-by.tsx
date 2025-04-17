4c8ba1b3fafd2dbe240f1e34e2778d70128cc9e2
solomonadzape <adzapesolomon@gmail.com>
2025-04-17 19:51:30 +0100

    Refactor TrustedBy component layout and structure for improved readability and organization

--- src/components/landing/trusted-by.tsx
+++ src/components/landing/trusted-by.tsx
@@ -1,29 +1,75 @@
 const companies = [
-  { name: "Google", logo: "/icons/google.png" },
-  { name: "Siemens", logo: "/icons/siemens.png" },
-  { name: "Microsoft", logo: "/icons/microsoft.png" },
-  { name: "Slack", logo: "/icons/slack.png" },
-  { name: "Tesla", logo: "/icons/tesla.png" },
-  { name: "Walmart", logo: "/icons/walmart.png" },
-  { name: "Adobe", logo: "/icons/adobe.png" },
-  { name: "Framer", logo: "/icons/framer.png" },
-  { name: "Openai", logo: "/icons/openai.png" },
+  [
+    {
+      name: "google",
+      logo: "/icons/google.png",
+    },
+    {
+      name: "siemens",
+      logo: "/icons/siemens.png",
+    },
+    {
+      name: "microsoft",
+      logo: "/icons/microsoft.png",
+    },
+    {
+      name: "slack",
+      logo: "/icons/slack.png",
+    },
+    {
+      name: "tesla",
+      logo: "/icons/tesla.png",
+    },
+  ],
+  [
+    {
+      name: "walmart",
+      logo: "/icons/walmart.png",
+    },
+    {
+      name: "adobe",
+      logo: "/icons/adobe.png",
+    },
+    {
+      name: "framer",
+      logo: "/icons/framer.png",
+    },
+    {
+      name: "openai",
+      logo: "/icons/openai.png",
+    },
+  ],
 ];
+
 export default function TrustedBy() {
   return (
-    <div className="my-20 md:my-28 w-full">
-      <div className="flex flex-col items-center justify-center gap-y-3 px-1 md:px-10">
-        <h3 className="font-semibold font-manrope">
-          Trusted by Over 5,500+ Industry Leaders
-        </h3>
-        <section className="flex justify-center items-ceneter flex-wrap gap-5 md:gap-x-16 md:max-w-[80vw]">
-          {companies.map((company) => (
-            <span className="w-1/6">
-              <img src={company.logo} alt={company.name} />
-            </span>
-          ))}
-        </section>
+    <section className="pt-24 pb-12 w-full font-manrope">
+      <div className="text-center text-2xl font-medium">
+        <h3>Trusted by Over 5,500+ Industry Leaders</h3>
+
+        <div className="flex flex-col items-center w-full max-w-6xl mx-auto">
+          <div className="flex items-center justify-around w-full">
+            {companies[0].map((company, key) => (
+              <img
+                key={key}
+                src={company.logo}
+                alt={company.name}
+                className="h-12 md:h-32"
+              />
+            ))}
+          </div>
+          <div className="flex items-center justify-center gap-12 -translate-y-1/2">
+            {companies[1].map((company, key) => (
+              <img
+                key={key}
+                src={company.logo}
+                alt={company.name}
+                className="h-12 md:h-32"
+              />
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
