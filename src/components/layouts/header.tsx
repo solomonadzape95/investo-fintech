236206de57bed1cbe58e5993e8d515cc624ce678
solomonadzape <adzapesolomon@gmail.com>
2025-04-22 21:52:06 +0100

    edits

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FaTimes, FaTimesCircle } from "react-icons/fa";
+import { FaTimesCircle } from "react-icons/fa";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
